2e96de5f101531701a8c2aedc160015bb49e5baf
tamzalit <bobby@192.168.33.10>
2020-04-15 03:48:41 +0200

    defi0

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ const express = require('express')
 const app = express()
 
 const port = 3000;
-const url = "https://blockchain.info/rawblock/"
-// /block/000000000002de92d93fcb92eeb2be097af8570a70fa5a8c6df473626891c9d6
+const url = "https://blockchain.info/rawblock/";
+const block = "000000000002de92d93fcb92eeb2be097af8570a70fa5a8c6df473626891c9d6";
 
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+
 
 app.get('/block/:block', function (req, res, next) {
+
+	console.log(req.params);
+
 	let block = req.params.block;
 	let blockJSON = {}
-
+	//console.log(url);
+  //next();
 	https.get( url + block, response => {
 	    let resultat = "";
 	    response.on("data", data => {
@@ -41,3 +43,30 @@ app.get('/block/:block', function (req, res, next) {
 
 })
 
+app.get('/', function (req, res) {
+
+	
+
+	https.get( url + block, response => {
+	    let resultat = "";
+	    response.on("data", data => {
+	        resultat += data;
+		})
+		
+		
+	    response.on('end', () =>{
+	        let blockJSON = JSON.parse(resultat)
+	        let str = 'Block N° : '+block+'<br>Date : '+new Date(blockJSON.time*1000)+'<br>'
+	        	str += 'Hauteur : '+blockJSON.height+'<br>'
+	        	for (let i=0,j=blockJSON.tx.length;i<j;i++){
+	        		str += 'Hash '+(i+1)+' : '+blockJSON.tx[i].hash+'<br>'
+	        	}
+	        	
+						
+	        console.log(blockJSON)
+
+        	res.send(JSON.stringify(str))
+	    })
+	})
+   
+	})
